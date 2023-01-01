5f2f2319d8c03f27f8be30dec5f990a8b7b415fd
Huseynbaba <hrzaquliyev@std.beu.edu.az>
2023-01-01 21:13:56 +0400

    [V1.0] users fragment added

---
+++ app/src/main/java/com/thenexprojects/firstjetpacksample/ui/model/User.kt
@@ -0,0 +1,9 @@
+package com.thenexprojects.firstjetpacksample.ui.model
+
+data class User(
+    var id: Int? = null,
+    var username: String,
+    var name: String,
+    var surname: String,
+    var age: String
+)
